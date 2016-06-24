0362894b5c7c27b0f2e4e4660faddc932464f107
ltw1994 <ltw2001@hanmail.net>
2016-06-24 15:16:29 +0900

    friend.txt read

--- Project/Project/main.c
+++ Project/Project/main.c
@@ -10,6 +10,28 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    FILE  *frendFile = freopen( "/Users/kimmyongjoon/Desktop/lwt/ltw1994/Project/Project/friend.txt", "r" ,stdin);
+    
+    if( frendFile != NULL )
+    {
+        char strTemp[255];
+        char *pStr;
+        
+        while( !feof( frendFile ) )
+        {
+            pStr = fgets( strTemp, sizeof(strTemp), frendFile );
+            printf( "%s", strTemp );
+            //printf( "%s", pStr );
+        }
+        fclose( frendFile );
+    }
+    else
+    {
+        //에러 처리
+    }
+    
+    
+    
     printf("test\n");
     return 0;
 }
