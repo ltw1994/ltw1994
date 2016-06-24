f118367a00d09d90836d2c6b39f3499b96ee88c7
Myeongjoon <kimmj8409@gmail.com>
2016-06-24 14:49:29 +0900

    Initial Project add

---
+++ Project/Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project
+//
+//  Created by kim myongjoon on 2016. 6. 24..
+//  Copyright © 2016년 itw1994. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
